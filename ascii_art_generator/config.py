963fb53b00af10534a4ebb29fe24df18d728082c
Nazar Kuryshchuk <kuryshchuk@Nazars-Laptop.local>
2024-10-17 17:19:06 +0300

    add change

--- ascii_art_generator/config.py
+++
@@ -1,7 +0,0 @@
-from colorama import Fore
-
-COLOR_MAP = {
-    'red': Fore.RED,
-    'green': Fore.GREEN,
-    'blue': Fore.BLUE
-}
--- ascii_art_generator/preview.py
+++
@@ -1,6 +0,0 @@
-def preview_art(ascii_art):
-    if ascii_art:
-        print("\nПопередній перегляд:")
-        print(ascii_art)
-    else:
-        print("ASCII-арт не вдалося створити, тому попередній перегляд неможливий.")
