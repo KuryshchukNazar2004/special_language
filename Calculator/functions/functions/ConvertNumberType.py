2cc989709cf1517af96d7919896dd08a08107f70
Nazar Kuryshchuk <kuryshchuk@Nazars-Laptop.local>
2024-10-17 12:52:01 +0300

    Remove Calculator project from lab3

--- Calculator/functions/functions/ConvertNumberType.py
+++
@@ -1,9 +0,0 @@
-def ConvertNumberType(nums):
-    nums = nums.replace(',', '.')
-    try:
-        number = float(nums)
-        if number.is_integer():
-            return int(number)
-        return number
-    except ValueError:
-        raise ValueError(f"Invalid number: '{nums}'")
