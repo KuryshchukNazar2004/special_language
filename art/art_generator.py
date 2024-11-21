2b877bf8d93aa9e9eb2b46ea9a8e9521d00261b2
KuryshchukNazar2004 <nazar.kuryshchuk@kpk-lp.com.ua>
2024-11-21 22:44:04 +0200

    add comments

--- art/art_generator.py
+++ art/art_generator.py
@@ -1,8 +1,28 @@
 from art.config import fonts, colors
 
 def generate_ascii_art(text, symbol, width, height, color_choice, alignment):
+    """
+    Генерує ASCII-арт з тексту з можливістю вибору символу, кольору, вирівнювання та розмірів.
+
+    Параметри:
+    - text (str): Текст, який буде перетворений в ASCII-арт.
+    - symbol (str): Символ, який використовується для заповнення пустих місць в ASCII-арті.
+    - width (int): Ширина кожного символа в ASCII-арті.
+    - height (int): Висота ASCII-арту.
+    - color_choice (str): Колір для ASCII-арту (вибирається з наданого словника `colors`).
+    - alignment (str): Вирівнювання тексту. Може бути 'center' (по центру) або 'right' (праворуч).
+
+    Повертає:
+    - (str): ASCII-арт у вигляді строки з кольоровим текстом.
+    
+    Викидає:
+    - KeyError: Якщо вказаний колір або символ не знайдені в словниках `fonts` або `colors`.
+    """
+
+    # Створення пустих рядків для кожного рівня висоти ASCII-арту
     ascii_art_lines = ['' for _ in range(height)]
     
+    # Перетворення кожного символу тексту на його ASCII-арт
     for char in text.upper():
         if char in fonts['standard']:
             char_lines = fonts['standard'][char]
@@ -13,6 +33,7 @@ def generate_ascii_art(text, symbol, width, height, color_choice, alignment):
                 else:
                     ascii_art_lines[i] += ' ' * (width + 1)  # Заповнюємо рядок пробілами
 
+    # Отримуємо код кольору з вибраного кольору
     color_code = colors.get(color_choice, colors['black_and_white'])
     
     # Вирівнювання тексту
@@ -23,4 +44,6 @@ def generate_ascii_art(text, symbol, width, height, color_choice, alignment):
 
     # Додаємо кольори до кожного рядка
     ascii_art_with_color = [color_code + line for line in ascii_art_lines]
+    
+    # Повертаємо згенерований ASCII-арт з кольорами
     return '\n'.join(ascii_art_with_color) + colors['black_and_white']
