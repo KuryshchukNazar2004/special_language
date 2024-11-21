085609ee5aa386a9a64c36d04fbb1a96f87812ae
KuryshchukNazar2004 <nazar.kuryshchuk@kpk-lp.com.ua>
2024-11-21 22:52:41 +0200

    add comments

--- main.py
+++ main.py
@@ -1,70 +1,103 @@
 from visualization import DataVisualizer
 
 def main():
+    """
+    Основний метод, який надає користувачу вибір різних типів візуалізацій даних:
+    лінійний графік, стовпчикова діаграма, діаграма розсіювання, гістограма та секторна діаграма.
+
+    Використовує клас DataVisualizer для завантаження і візуалізації даних з CSV файлу.
+    Запитує вхідні параметри від користувача і виводить відповідну візуалізацію.
+    """
+    # Шлях до даних
     data_path = "data/sales_data.csv"
+    # Ініціалізуємо візуалізатор
     visualizer = DataVisualizer(data_path)
 
-    print("Виберіть тип візуалізації:")
-    print("1. Лінійний графік")
-    print("2. Стовпчикова діаграма")
-    print("3. Діаграма розсіювання")
-    print("4. Гістограма")
-    print("5. Секторна діаграма")
-    print("6. Вихід")
+    while True:
+        print("Виберіть тип візуалізації:")
+        print("1. Лінійний графік")
+        print("2. Стовпчикова діаграма")
+        print("3. Діаграма розсіювання")
+        print("4. Гістограма")
+        print("5. Секторна діаграма")
+        print("6. Вихід")
 
-    choice = input("Введіть номер: ")
+        # Отримуємо вибір користувача
+        choice = input("Введіть номер: ")
 
-    if choice == "1":
-        print("Доступні стовпці для візуалізації:")
-        print(visualizer.data.columns.tolist()) 
+        if choice == "1":
+            """
+            Створення лінійного графіка. Запитує у користувача стовпці для осі X та осі Y,
+            а також додаткові параметри для побудови графіка (назва, підписи осей).
+            """
+            print("Доступні стовпці для візуалізації:")
+            print(visualizer.data.columns.tolist())
 
-        x_column = input("Введіть назву стовпця для осі X (наприклад, 'Date'): ") 
-        y_column = input("Введіть назву стовпця для осі Y (наприклад, 'Sales'): ")
-        title = input("Введіть назву графіка: ")
-        xlabel = input("Введіть назву осі X: ")
-        ylabel = input("Введіть назву осі Y: ")
-        visualizer.plot_line_chart(x_column, y_column, title, xlabel, ylabel)
-    elif choice == "2":
-        print("Доступні стовпці для візуалізації:")
-        print(visualizer.data.columns.tolist()) 
+            x_column = input("Введіть назву стовпця для осі X (наприклад, 'Date'): ")
+            y_column = input("Введіть назву стовпця для осі Y (наприклад, 'Sales'): ")
+            title = input("Введіть назву графіка: ")
+            xlabel = input("Введіть назву осі X: ")
+            ylabel = input("Введіть назву осі Y: ")
+            visualizer.plot_line_chart(x_column, y_column, title, xlabel, ylabel)
+        elif choice == "2":
+            """
+            Створення стовпчикової діаграми. Запитує у користувача стовпці для осі X та осі Y,
+            а також додаткові параметри для побудови діаграми (назва, підписи осей).
+            """
+            print("Доступні стовпці для візуалізації:")
+            print(visualizer.data.columns.tolist())
 
-        x_column = input("Введіть назву стовпця для осі X: ")
-        y_column = input("Введіть назву стовпця для осі Y: ")
-        title = input("Введіть назву графіка: ")
-        xlabel = input("Введіть назву осі X: ")
-        ylabel = input("Введіть назву осі Y: ")
-        visualizer.plot_bar_chart(x_column, y_column, title, xlabel, ylabel)
-    elif choice == "3":
-        print("Доступні стовпці для візуалізації:")
-        print(visualizer.data.columns.tolist()) 
+            x_column = input("Введіть назву стовпця для осі X: ")
+            y_column = input("Введіть назву стовпця для осі Y: ")
+            title = input("Введіть назву графіка: ")
+            xlabel = input("Введіть назву осі X: ")
+            ylabel = input("Введіть назву осі Y: ")
+            visualizer.plot_bar_chart(x_column, y_column, title, xlabel, ylabel)
+        elif choice == "3":
+            """
+            Створення діаграми розсіювання. Запитує у користувача стовпці для осі X та осі Y,
+            а також додаткові параметри для побудови діаграми (назва, підписи осей).
+            """
+            print("Доступні стовпці для візуалізації:")
+            print(visualizer.data.columns.tolist())
 
-        x_column = input("Введіть назву стовпця для осі X: ")
-        y_column = input("Введіть назву стовпця для осі Y: ")
-        title = input("Введіть назву графіка: ")
-        xlabel = input("Введіть назву осі X: ")
-        ylabel = input("Введіть назву осі Y: ")
-        visualizer.plot_scatter_chart(x_column, y_column, title, xlabel, ylabel)
-    elif choice == "4":
-        print("Доступні стовпці для візуалізації:")
-        print(visualizer.data.columns.tolist()) 
+            x_column = input("Введіть назву стовпця для осі X: ")
+            y_column = input("Введіть назву стовпця для осі Y: ")
+            title = input("Введіть назву графіка: ")
+            xlabel = input("Введіть назву осі X: ")
+            ylabel = input("Введіть назву осі Y: ")
+            visualizer.plot_scatter_chart(x_column, y_column, title, xlabel, ylabel)
+        elif choice == "4":
+            """
+            Створення гістограми. Запитує у користувача стовпець для візуалізації і додаткові параметри.
+            """
+            print("Доступні стовпці для візуалізації:")
+            print(visualizer.data.columns.tolist())
 
-        column = input("Введіть назву стовпця: ")
-        title = input("Введіть назву гістограми: ")
-        xlabel = input("Введіть назву осі X: ")
-        ylabel = input("Введіть назву осі Y: ")
-        visualizer.plot_histogram(column, title, xlabel, ylabel)
-    elif choice == "5":
-        print("Доступні стовпці для візуалізації:")
-        print(visualizer.data.columns.tolist()) 
+            column = input("Введіть назву стовпця: ")
+            title = input("Введіть назву гістограми: ")
+            xlabel = input("Введіть назву осі X: ")
+            ylabel = input("Введіть назву осі Y: ")
+            visualizer.plot_histogram(column, title, xlabel, ylabel)
+        elif choice == "5":
+            """
+            Створення секторної діаграми. Запитує у користувача стовпець для візуалізації.
+            """
+            print("Доступні стовпці для візуалізації:")
+            print(visualizer.data.columns.tolist())
 
-        column = input("Введіть назву стовпця: ")
-        title = input("Введіть назву секторної діаграми: ")
-        visualizer.plot_pie_chart(column, title)
-    elif choice == "6":
-        print("Вихід")
-        exit()
-    else:
-        print("Неправильний вибір.")
+            column = input("Введіть назву стовпця: ")
+            title = input("Введіть назву секторної діаграми: ")
+            visualizer.plot_pie_chart(column, title)
+        elif choice == "6":
+            """
+            Завершує програму.
+            """
+            print("Вихід")
+            break
+        else:
+            print("Неправильний вибір.")
+            
 
 if __name__ == "__main__":
     main()
--- visualization.py
+++ visualization.py
@@ -2,10 +2,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class DataVisualizer:
+    """
+    Клас для візуалізації даних з CSV файлу за допомогою різних типів графіків.
+    Використовує бібліотеки pandas для обробки даних і matplotlib для створення візуалізацій.
+
+    Атрибути:
+        data (pd.DataFrame): Дані з CSV файлу, який вказано при створенні об'єкта.
+
+    Методи:
+        plot_line_chart(x_column, y_column, title, xlabel, ylabel):
+            Створює лінійний графік на основі заданих стовпців для осей X і Y.
+
+        plot_bar_chart(x_column, y_column, title, xlabel, ylabel):
+            Створює стовпчикову діаграму на основі заданих стовпців для осей X і Y.
+
+        plot_scatter_chart(x_column, y_column, title, xlabel, ylabel):
+            Створює діаграму розсіювання на основі заданих стовпців для осей X і Y.
+
+        plot_histogram(column, title, xlabel, ylabel):
+            Створює гістограму для одного стовпця даних.
+
+        plot_pie_chart(column, title):
+            Створює секторну діаграму для одного стовпця даних, що містить категорії.
+    """
+
     def __init__(self, data_path):
+        """
+        Ініціалізує об'єкт класу DataVisualizer і завантажує дані з CSV файлу.
+
+        Параметри:
+            data_path (str): Шлях до CSV файлу, який містить дані для візуалізації.
+        """
         self.data = pd.read_csv(data_path)
 
     def plot_line_chart(self, x_column, y_column, title, xlabel, ylabel):
+        """
+        Створює лінійний графік на основі заданих стовпців для осей X і Y.
+
+        Параметри:
+            x_column (str): Назва стовпця для осі X.
+            y_column (str): Назва стовпця для осі Y.
+            title (str): Назва графіка.
+            xlabel (str): Назва осі X.
+            ylabel (str): Назва осі Y.
+        """
         plt.plot(self.data[x_column], self.data[y_column])
         plt.title(title)
         plt.xlabel(xlabel)
@@ -13,6 +53,16 @@ class DataVisualizer:
         plt.show()
 
     def plot_bar_chart(self, x_column, y_column, title, xlabel, ylabel):
+        """
+        Створює стовпчикову діаграму на основі заданих стовпців для осей X і Y.
+
+        Параметри:
+            x_column (str): Назва стовпця для осі X.
+            y_column (str): Назва стовпця для осі Y.
+            title (str): Назва графіка.
+            xlabel (str): Назва осі X.
+            ylabel (str): Назва осі Y.
+        """
         plt.bar(self.data[x_column], self.data[y_column])
         plt.title(title)
         plt.xlabel(xlabel)
@@ -20,13 +70,32 @@ class DataVisualizer:
         plt.show()
 
     def plot_scatter_chart(self, x_column, y_column, title, xlabel, ylabel):
+        """
+        Створює діаграму розсіювання на основі заданих стовпців для осей X і Y.
+
+        Параметри:
+            x_column (str): Назва стовпця для осі X.
+            y_column (str): Назва стовпця для осі Y.
+            title (str): Назва графіка.
+            xlabel (str): Назва осі X.
+            ylabel (str): Назва осі Y.
+        """
         plt.scatter(self.data[x_column], self.data[y_column])
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.show() 
+        plt.show()
 
     def plot_histogram(self, column, title, xlabel, ylabel):
+        """
+        Створює гістограму для одного стовпця даних.
+
+        Параметри:
+            column (str): Назва стовпця для побудови гістограми.
+            title (str): Назва гістограми.
+            xlabel (str): Назва осі X.
+            ylabel (str): Назва осі Y.
+        """
         plt.hist(self.data[column])
         plt.title(title)
         plt.xlabel(xlabel)
@@ -34,6 +103,13 @@ class DataVisualizer:
         plt.show()
 
     def plot_pie_chart(self, column, title):
+        """
+        Створює секторну діаграму для одного стовпця даних, що містить категорії.
+
+        Параметри:
+            column (str): Назва стовпця для побудови секторної діаграми.
+            title (str): Назва секторної діаграми.
+        """
         data_counts = self.data[column].value_counts()
         plt.pie(data_counts.values, labels=data_counts.index, autopct='%1.1f%%')
         plt.title(title)
